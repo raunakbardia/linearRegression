438765d5ae40a717c077f83a03022d4fd277a0e9
raunakbardia <raunakbardia@gmail.com>
2020-04-22 16:38:31 +0530

    First R script completed.

---
+++ firstScript_scatterPlot_BB1.R
@@ -0,0 +1,9 @@
+library(Lahman)
+library(tidyverse)
+library(dslabs)
+ds_theme_set()
+
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(HR_per_game = HR / G, R_per_game = R / G) %>%
+  ggplot(aes(HR_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
